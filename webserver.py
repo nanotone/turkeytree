84461c2629f0ea2827dc4cdeb42db74ab60af9bb
nanotone <nanotone@gmail.com>
2012-11-22 18:02:43 -0500

    First pass at login stuff. Missing template.

--- webserver.py
+++ webserver.py
@@ -4,11 +4,21 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-	if 'email' in flask.session:
-		return "welcome %s" % (flask.session['email'])
+	if 'userid' in flask.session:
+		return "welcome %s" % (flask.session['userid'])
 	else:
-		return "please login or register"
+		return flask.render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	form = flask.request.form
+	if form['email'] == "let@me.in" and form['password'] == "letmein":
+		flask.session['userid'] = "hello"
+		return flask.redirect(flask.url_for('index'))
+	else:
+		return flask.render_template('login.html')
 
 if __name__ == '__main__':
+	app.secret_key = open('secret_key').read()
 	app.run(debug=True, host='0.0.0.0')
 
