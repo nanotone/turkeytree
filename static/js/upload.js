374c1c2ca2667b311dbbea19c03dac8e4b423a77
Tina Ye <t@tinabeans.com>
2012-11-24 17:12:14 -0500

    added a progressbar to thumbnail uploads! yeah!

--- static/js/upload.js
+++ static/js/upload.js
@@ -4,15 +4,39 @@ function abortEvent(e) {
 	e.preventDefault();
 	e.stopPropagation();
 }
-function previewImage(file) {
-	var img = document.createElement('img');
-	document.body.appendChild(img);
-	var reader = new FileReader();
+
+function previewImage(file, i) {
+	var $thumbContainer = $('<div class="upload-thumb" id="thumb"' + i + '"><img /><div class="progressbar"></div></div>');
+	$('body').append($thumbContainer);
+	var reader = new FileReader(); // transforms the image into a data url
 	reader.onload = function(e) {
-		img.src = e.target.result;
-		img[img.width > img.height ? 'width':'height'] = 100;
+		var $img = $thumbContainer.children('img');
+		$img.attr('src', e.target.result);
+		$thumbContainer[$img.width() > $img.height() ? 'width':'height'](150); // voodoo.
 	};
 	reader.readAsDataURL(file);
+
+	xhr = new XMLHttpRequest();
+	xhr.open('post', '/photo', true);
+	var formData = new FormData();
+	formData.append('photo', file);
+	//formData.append('tzoffset', (new Date()).getTimezoneOffset());
+
+	var eventSource = xhr.upload || xhr;
+
+	eventSource.addEventListener("progress", function(e) {
+		 console.log("anything");
+
+	    var position = e.position || e.loaded;
+	    var total = e.totalSize || e.total;
+	    var percent = position/total * 50;
+
+
+	    
+	    $thumbContainer.children('.progressbar').width(percent + '%');
+	});
+
+	xhr.send(formData);
 }
 
 
@@ -21,13 +45,7 @@ function dropHandler(e) {
 	e.stopPropagation();
 	var files = e.dataTransfer.files;
 	for (var i = 0; i < files.length; i++) {
-		previewImage(files[i]);
-		xhr = new XMLHttpRequest();
-		xhr.open('post', '/photo', true);
-		var formData = new FormData();
-		formData.append('photo', files[i]);
-		//formData.append('tzoffset', (new Date()).getTimezoneOffset());
-		xhr.send(formData);
+		previewImage(files[i], i);
 	}
 }
 function dragEnterHandler(e) {
